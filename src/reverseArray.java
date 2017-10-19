d59a0ca3aaa36c91e003e878bf6152f92310abd4
ericadavis <ericadavis@172.16.21.33>
2017-10-19 19:40:33 -0400

    Done!

---
+++ src/reverseArray.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+
+public class reverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] array1 = {1,2,3,4,5,6,7,8,9};
+		int store= 0;
+		int marker= array1.length;
+		for(int i =1; 0 < marker/2; i++) {
+			store =array1[array1.length-i];
+			array1[array1.length-i] = array1[i-1];
+			array1[i-1]= store;
+			marker-=2;
+		}
+		for(int i=0; i < array1.length; i++) {
+			System.out.print(array1[i]);
+		}
+	}
+
+}
